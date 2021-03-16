1d7eebcd043393675c1996ee4b4fedcd5af149ea
Lucas Knudsen <laksteelhouse@gmail.com>
2021-03-16 20:45:23 +0100

    Sets up main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import React from 'react'
+import Balance from './components/Balance'
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="frame">
+        <div className="screen">
+          <h3>Thank you, please come again!</h3>
+          <input
+            name="amount"
+            type="number"
+            placeholder="Amount..."
+          />
+          <Balance />
+        </div>
+        <div className="buttons">
+          <button>Withdraw</button>
+          <button>Deposit</button>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/Balance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <div>
+      <h2>Your balance: 1000;-</h2>
+    </div>
+  )
+}
+
+export default Balance
